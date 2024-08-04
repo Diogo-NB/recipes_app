8f8f506397dc49f8d99cc94583965fa895da06b7
Diogo-NB <diogonunes.20022@gmail.com>
2024-08-04 17:08:56 -0300

    Add categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -5,6 +5,32 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick a category'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: const [
+          Text(
+            '1',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            '2',
+            style: TextStyle(color: Colors.white),
+          ),
+          Text(
+            '3',
+            style: TextStyle(color: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
 }
